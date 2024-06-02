df3c136ab9440fea3ae4080cdc05d0685b98be71
Bhavani Shankar <komatibhavanishankar@gmail.com>
2024-06-02 16:10:45 +0530

    added 8

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h5*x=n!av-6fa%ljy_4)=_!s7(e0040_rkz363c_g8gawrydmw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
